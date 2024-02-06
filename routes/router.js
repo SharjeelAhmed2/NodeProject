0fd1a34ddf5aeb32c8f0f0aca16691f2f8e71bea
SharjeelAhmed2 <sharjeelahmed614@gmail.com>
2024-02-06 23:37:30 +0500

    Added Controllers

--- routes/router.js
+++ routes/router.js
@@ -2,62 +2,17 @@
 const express = require("express");
 const Blog = require("../models/blogsdata");
 
+const blogController = require('../controllers/blogcontroller');
+
+
 const router = express.Router();
 /// Show data on FrontEnd
 
-router.get('/home',(req,res)=>
-{
-  // the find is an asynchronous task so we wait for this hence then and catch
-  Blog.find().then((result)=>
-  {
-    res.render('index', { title: 'Systems Limited', blogs: result});
-  }).catch((err)=>{
-    console.log(err);
-  }
-  )
-})
+router.get('/blogs/create', blogController.blog_create_get);
+router.get('/home', blogController.blog_index);
+router.post('/createBlogs', blogController.blog_create_post);
+router.get('/blogs/:id', blogController.blog_details);
+router.delete('/blogs/:id', blogController.blog_delete);
 
-  // Create a new blog page
-  router.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
-  
-/// Making a post request 
-
-router.post('/createBlogs',(req,res)=>
-{
-    const newBlog = new Blog(req.body);
-
-    newBlog.save()
-    .then((result)=>
-    {
-      res.redirect('/home');
-    }).catch((err) => {
-      console.log(err);
-    });
-})
-
-/// Making a get request that will render content of a single blog on a page
-
-router.get('/blogs/:id', (req,res) =>{
-  // save id in a variable
-  const id = req.params.id;
-  console.log(id);
-  Blog.findById(id).then((result)=>
-{
-  res.render('details', {blog:result, title: 'Blog Details'})
-}).catch((err)=>{console.log(err);})
-})
-
-
-/// delete request
-
-router.delete('/blogs/:id', (req,res) => {
-   const id = req.params.id;
-
-   Blog.findByIdAndDelete(id)
-   .then((result) => {res.json({redirect: '/home'})})
-   .catch((err)=>{console.log(err)});
-})
 
 module.exports = router;
